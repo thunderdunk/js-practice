e37d350b98ec546e4a9b477d4c0c6f37a5e0c2c9
Dan Sweet <dan@heptagoncreative.com>
2016-07-14 13:14:46 -0700

    Setup sandbox

---
+++ sandbox/app.js
@@ -0,0 +1,62 @@
+// Create div element
+divEl = document.createElement( 'div' );
+docFrag = document.createDocumentFragment();
+
+// Set class for div element
+divEl.setAttribute( 'class', 'new-element' );
+
+// Paragraph 1
+a1Text = document.createTextNode( 'here' );
+a1El = document.createElement( 'a' );
+p1Text1 = document.createTextNode( 'I like dinos, as you can see ' );
+p1Text2 = document.createTextNode( '.' );
+p1El = document.createElement( 'p' );
+
+a1El.appendChild(a1Text);
+a1El.setAttribute( 'href', '#somewhere' );
+
+p1El.appendChild( p1Text1 );
+p1El.appendChild( a1El );
+p1El.appendChild( p1Text2 );
+
+// Paragraph 2
+p2Text = document.createTextNode( 'Here is why:' );
+p2El = document.createElement( 'p' );
+
+p2El.appendChild( p2Text );
+
+// List
+li1Text = document.createTextNode( 'They are big' );
+li1El = document.createElement( 'li' );
+li2Text = document.createTextNode( 'They are cool' );
+li2El = document.createElement( 'li' );
+ul1El = document.createElement( 'ul' );
+
+li1El.appendChild( li1Text );
+li2El.appendChild( li2Text );
+ul1El.appendChild( li1El );
+ul1El.appendChild( li2El );
+
+
+// Put it together
+docFrag.appendChild( p1El );
+docFrag.appendChild( p2El );
+docFrag.appendChild( ul1El );
+
+
+divEl.appendChild( docFrag );
+
+console.log( divEl );
+
+
+stegoSection = document.getElementById( 'stegosaurus' );
+stegoSection.outerHTML = stegoSection.outerHTML + divEl;
+
+// <div class="new-element">
+//   <p>I like dinos, as you can see <a href="#somewhere">here</a>.</p>
+//   <p>Here is why:</p>
+//   <ul>
+//     <li>They are big</li>
+//     <li>They are cool</li>
+//   </ul>
+// </div>
