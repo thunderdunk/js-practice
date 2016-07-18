2f474cc093901f2f3ad2e22a57e7599bd53abb8a
dansweet <dan@heptagoncreative.com>
2016-07-18 09:06:00 -0700

    update file

--- sandbox/app.js
+++ sandbox/app.js
@@ -43,14 +43,19 @@ docFrag.appendChild( p1El );
 docFrag.appendChild( p2El );
 docFrag.appendChild( ul1El );
 
-
+// Append our frag to the outer div
 divEl.appendChild( docFrag );
 
 console.log( divEl );
 
+masthead = document.querySelector( '#masthead' );
+insertLocation = masthead.children[1];
 
 stegoSection = document.getElementById( 'stegosaurus' );
-stegoSection.outerHTML = stegoSection.outerHTML + divEl;
+masthead.replaceChild( divEl, insertLocation );
+// stegoSection.insertBefore( divEl,  );
+
+console.log( stegoSection.outerHTML );
 
 // <div class="new-element">
 //   <p>I like dinos, as you can see <a href="#somewhere">here</a>.</p>
@@ -60,3 +65,91 @@ stegoSection.outerHTML = stegoSection.outerHTML + divEl;
 //     <li>They are cool</li>
 //   </ul>
 // </div>
+
+
+/**
+ * Clone those list items!
+ */
+
+var dinoList = document.querySelector( '.fields' ),
+    firstItem = dinoList.children[0],
+    firstFieldLabel = firstItem.querySelector( 'label' ),
+    firstFieldLabelText = firstFieldLabel.textContent,
+    firstFieldInput = firstItem.querySelector( 'input' ),
+    firstFieldIdStr = firstFieldInput.getAttribute( 'id' ),
+
+
+
+    firstFieldPlaceholder = firstFieldInput.getAttribute( 'placeholder' ),
+
+    secondItem = firstItem.cloneNode( true ),
+    secondFieldLabel = secondItem.querySelector( 'label' ),
+    secondFieldInput = secondItem.querySelector( 'input' ),
+    secondFieldIdStr = updateIdStr( firstFieldIdStr ),
+
+    secondFieldPlaceholder = updatePlaceholderStr( firstFieldPlaceholder );
+    secondFieldLabelText = updateLabelStr( firstFieldLabelText );
+
+    // console.log( firstFieldLabelText );
+
+    // console.log( secondFieldIdStr );
+
+
+function updateIdStr( value ) {
+  var strArray = value.split( '-' ),
+      id = strArray[ strArray.length - 1 ],
+      newId = parseInt( id ) + 1
+
+    // console.log( strArray );
+    // console.log( id );
+    // console.log( newId );
+
+  strArray[ strArray.length - 1 ] = newId;
+    // console.log( strArray );
+    return strArray.toString().replace( ',', '-' );
+}
+
+function updatePlaceholderStr( value ) {
+  var strArray = value.split( ' ' ),
+      placeholder = strArray[ strArray.length - 1 ],
+      newPlaceholder = parseInt( placeholder ) + 1
+
+      console.log( strArray );
+      console.log( placeholder );
+      console.log( newPlaceholder );
+
+  strArray[ strArray.length - 1 ] = newPlaceholder;
+  // console.log( strArray.toString().replace( /,/g, ' ' ) );
+  // Using some sort of RegEx flag to replace all separators instead of just the first
+  // http://stackoverflow.com/questions/10610402/javascript-replace-all-commas-in-a-string
+  return strArray.toString().replace( /,/g, ' ' );
+}
+
+
+
+function updateLabelStr( textContent ) {
+  var strArray = textContent.split( ' ' ),
+      labelText = strArray[ strArray.length - 1 ],
+      newLabelText = parseInt( labelText ) + 1
+
+      console.log( strArray );
+      console.log( labelText );
+      console.log( newLabelText );
+
+  strArray[ strArray.length - 1 ] = newLabelText;
+  console.log( strArray.toString().replace( /,/g, ' ' ) );
+  return strArray.toString().replace( /,/g, ' ' );
+}
+
+
+secondFieldLabel.setAttribute( 'for', secondFieldIdStr );
+secondFieldInput.setAttribute( 'id', secondFieldIdStr );
+secondFieldInput.setAttribute( 'name', secondFieldIdStr );
+
+secondFieldInput.setAttribute( 'placeholder', secondFieldPlaceholder );
+
+secondFieldLabel.textContent = secondFieldLabelText;
+
+dinoList.appendChild( secondItem );
+
+// secondFieldLabel.appendChild( secondFieldNewText );
